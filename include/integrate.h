4419cef3e39a5cf048c752ac4865817b15bb6498
Zach Raines <raineszm@gmail.com>
2018-05-15 23:15:00 -0400

    Remove old integration code

--- include/integrate.h
+++ include/integrate.h
@@ -3,47 +3,15 @@
 #include <memory>
 
 #include <gsl/gsl_integration.h>
-#include <rzmcmt/integrate.h>
 
-template<class F>
-class Integrand
+const double EPSABS = 1e-8;
+const double EPSREL = 1e-5;
+
+using workspace = class IntegrationWorkspace
 {
-  const F _f;
-
-  double jac(double t) const { return 1 / (t * t); }
-
-  double z(double t) const { return (1 - t) / t; }
-
-public:
-  explicit Integrand(const F& f)
-    : _f(f)
-  {}
-
-  int operator()(double v[1], const double t[2]) const
-  {
-    auto zx = z(t[0]);
-    auto zy = z(t[1]);
-    v[0] = (_f(zx, zy) + _f(-zx, zy) + _f(zx, -zy) + _f(-zx, -zy)) * jac(t[0]) *
-           jac(t[1]);
-    return 0;
-  }
-};
-
-template<class F>
-double
-integrate(const F& f)
-{
-  auto i = Integrand(f);
-  auto [result, err] = rzmcmt::integrate<2, 1>(i);
-  return result[0] / (4 * M_PI * M_PI);
-}
-
-using workspace = std::unique_ptr<gsl_integration_workspace,
-                                  decltype(&gsl_integration_workspace_free)>;
-
-class IntegrationWorkspace
-{
-  workspace wsp;
+  std::unique_ptr<gsl_integration_workspace,
+                  decltype(&gsl_integration_workspace_free)>
+    wsp;
 
 public:
   IntegrationWorkspace(const size_t n = 1000)
@@ -110,35 +78,4 @@ gsl_xi_integrate(const F& f, double a)
     outer, a, EPSABS, EPSREL, limit, wsp2, &result, &abserr);
 
   return result;
-}
-
-template<class F>
-class XiIntegrand
-{
-  const F _f;
-  const double _a;
-
-public:
-  XiIntegrand(const F& f, double a)
-    : _f(f)
-    , _a(a)
-  {}
-
-  int operator()(double v[1], const double t[2]) const
-  {
-    auto x = _a + (1 - t[0]) / t[0];
-    auto jac = 1 / (t[0] * t[0]);
-    auto theta = 2 * M_PI * t[1];
-    v[0] = _f(x, theta) * jac;
-    return 0;
-  }
-};
-
-template<class F>
-double
-xi_integrate(const F& f, double a)
-{
-  auto i = XiIntegrand(f, a);
-  auto [result, err] = rzmcmt::integrate<2, 1>(i);
-  return result[0];
 }
