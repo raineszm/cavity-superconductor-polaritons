4a2e2e801beadde8f911696940ba9d7fd0dd86e5
Zach Raines <raineszm@gmail.com>
2018-05-25 13:42:38 -0400

    Rework exceptions

---
+++ include/exceptions.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <gsl/gsl_errno.h>
+
+#include <stdexcept>
+#include <string>
+
+namespace gsl {
+class GSLException : public std::runtime_error
+{
+
+public:
+  GSLException(const char* reason, const char* file, int line, int gsl_errno)
+    : std::runtime_error(gsl_strerror(gsl_errno) + std::string(reason))
+  {}
+};
+
+class RootException : public GSLException
+{
+public:
+  RootException(const char* reason, const char* file, int line, int gsl_errno)
+    : GSLException(reason, file, line, gsl_errno)
+  {}
+};
+
+[[noreturn]] inline void
+error_handler(const char* reason, const char* file, int line, int gsl_errno)
+{
+  if (gsl_errno == GSL_EBADFUNC || gsl_errno == GSL_EZERODIV) {
+    throw RootException(reason, file, line, gsl_errno);
+  } else {
+    throw GSLException(reason, file, line, gsl_errno);
+  }
+}
+}
--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,6 +1,7 @@
 /* #define EIGEN_STACK_ALLOCATION_LIMIT 0 */
 
 /* #include <pybind11/eigen.h> */
+#include "exceptions.h"
 #include <gsl/gsl_errno.h>
 #include <pybind11/pybind11.h>
 
@@ -22,17 +23,10 @@ bind_polariton(py::module&);
 void
 bind_cavity(py::module&);
 
-void
-handler(const char* reason, const char* file, int line, int gsl_errno)
-{
-  std::string msg(reason);
-  throw std::runtime_error("GSL: " + msg);
-}
-
 PYBIND11_MODULE(bardasis_schrieffer, m)
 {
   // gsl_set_error_handler_off();
-  gsl_set_error_handler(&handler);
+  gsl_set_error_handler(&gsl::error_handler);
   m.doc() =
     "Calculate the hybridization of photons and Bardasis-Schrieffer modes";
 
