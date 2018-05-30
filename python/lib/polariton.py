7db512880de1688c7f1de5bd2ab0472a7a3d3301
Zach Raines <raineszm@gmail.com>
2018-05-30 16:33:11 -0400

    Make bs mass units sane

--- python/lib/polariton.py
+++ python/lib/polariton.py
@@ -6,8 +6,8 @@ from materials import NIOBIUM
 def build_polariton(m, mu, Tc, vs, theta_v, T, big=1):
     sys = bsm.System(m, mu, Tc, vs, theta_v)
     state = bsm.State.solve(sys, T)
-    bs = bsm.BS(state.delta/10, state)
-    return bsm.Polariton(bs, bsm.Cavity(1.04*bs.root), bsm.Coupling(state), big=big)
+    bs = bsm.BS(0.3, state)
+    return bsm.Polariton(bs, bsm.Cavity(1.2*bs.root), bsm.Coupling(state), big=big)
 
 
 def niobium(vs, theta_v=0., Trel=0.5, big=1.):
