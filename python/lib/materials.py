d573fa919c89e8031205a5e96388155977d0564f
Zach Raines <raineszm@gmail.com>
2018-06-06 11:43:54 -0400

    Document python code a little more

--- python/lib/materials.py
+++ python/lib/materials.py
@@ -1,8 +1,15 @@
+"""
+Material parameters for various known superconductors.
+
+Units are converted to Gaussian atomic units
+"""
+
 from units import ureg, Q_
 
 # Atomic units in meV
+"""Material parameters for Niobium """
 NIOBIUM = dict(  # meV
-    Tc=Q_(9.5, 'kelvin').to('meV', 'boltzmann').magnitude,
-    mu=Q_(6.18e4, 'kelvin').to('meV', 'boltzmann').magnitude,
-    m=Q_(1.6, 'm_e').magnitude  # bare electron mass
+    Tc=Q_(9.5, "kelvin").to("meV", "boltzmann").magnitude,
+    mu=Q_(6.18e4, "kelvin").to("meV", "boltzmann").magnitude,
+    m=Q_(1.6, "m_e").magnitude,  # bare electron mass
 )
--- python/lib/units.py
+++ python/lib/units.py
@@ -1,8 +1,9 @@
-'''
+"""
 Unit conversion.
 
 We use Gaussian atomic units for these calculations
-'''
+"""
 import pint
-ureg = pint.UnitRegistry(system='cgs')
+
+ureg = pint.UnitRegistry(system="cgs")
 Q_ = ureg.Quantity
