b2da50cb75d1b39e83df1d8f0ae3718ca2c58f16
Zach Raines <raineszm@gmail.com>
2018-06-15 16:43:22 -0400

    Start stubbing out data generation

---
+++ python/lib/data.py
@@ -0,0 +1,42 @@
+import bardasis_schrieffer as bsm
+import csv
+import itertools as it
+import numpy as np
+import tqdm
+from pathlib import Path
+from polariton import niobium
+from materials import NIOBIUM
+
+
+TREL = 0.4  # T/T_c
+VREL = 0.6  # vs/v_c
+DIPOLEX = 100  # Phenominological enhancement factor
+ROOTREL = 0.95
+
+
+def vc():
+    sys0 = bardasis_schrieffer.System(vs=0., **NIOBIUM)
+    state0 = bardasis_schrieffer.State.solve(sys0, TREL * NIOBIUM["Tc"])
+    return (
+        state0.delta / sys0.kf
+    )  # The critical superfluid velocity via Landau's argument
+
+
+def qc(p: bsm.Polariton):
+    return 2 * p.state.delta / bsm.C
+
+
+def data(fname, qs, thetas):
+    p = niobium(
+        VREL * vc(), Trel=TREL, dipoleX=DIPOLEX, root_rel=ROOTREL, cls=bsm.ModePolariton
+    )
+    writer = csv.DictWriter(fname, ["q", "theta", "omega"])
+    writer.writeheader()
+    for (theta, q) in tqdm.tqdm(it.product(thetas, qs)):
+        qx = q * np.cos(theta)
+        qy = q * np.sin(theta)
+
+        modes = p.find_modes(qx, qy)
+        for m in modes:
+            writer.writerow({"q": q, "theta": theta, "omega": m})
+
