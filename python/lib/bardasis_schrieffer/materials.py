4339d2cdea175d1446d88935d106190f8a232a27
Zach Raines <raineszm@gmail.com>
2018-06-22 16:41:33 -0400

    Adjust things to work for meV

--- python/lib/bardasis_schrieffer/materials.py
+++ python/lib/bardasis_schrieffer/materials.py
@@ -6,10 +6,19 @@ Units are converted to Gaussian atomic units
 
 from .units import ureg, Q_
 
+SCALAR = Q_(1, "hartree").to("meV").magnitude
+
 # Atomic units in meV
 """Material parameters for Niobium """
 NIOBIUM = dict(  # meV
     Tc=Q_(9.5, "kelvin").to("meV", "boltzmann").magnitude,
     mu=Q_(6.18e4, "kelvin").to("meV", "boltzmann").magnitude,
-    m=Q_(1.6, "m_e").magnitude,  # bare electron mass
+    m=Q_(1.6, "m_e").magnitude * SCALAR,  # bare electron mass * meV/Hartree
+)
+
+"""Material parameters for a model of pnictides"""
+PNICTIDE = dict(  # meV
+    Tc=Q_(35, "kelvin").to("meV", "boltzmann").magnitude,
+    mu=Q_(100, "meV").magnitude,
+    m=Q_(0.7, "m_e").magnitude * SCALAR,  # bare electron mass * meV/Hartree
 )
