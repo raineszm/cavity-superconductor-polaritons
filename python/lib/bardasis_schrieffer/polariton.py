ad7ad66cb405cc011390229c4b0e3070d6365abc
Zach Raines <raineszm@gmail.com>
2018-07-05 14:31:24 -0400

    Define cavity frequency by theta_s bs root

--- python/lib/bardasis_schrieffer/polariton.py
+++ python/lib/bardasis_schrieffer/polariton.py
@@ -9,8 +9,11 @@ def _build_polariton(
     sys = bsm.System(m, mu, Tc, vs, theta_s)
     state = bsm.State.solve(sys, Trel * Tc)
     bs = bsm.BS(bsmass, state)
+    sys0 = bsm.System(m, mu, Tc, vs, 0)
+    state0 = bsm.State.solve(sys0, Trel * Tc)
+    bs0 = bsm.BS(bsmass, state0)
     return cls(
-        coupling=bsm.Coupling(cav=bsm.Cavity(bs.root * root_rel), bs=bs),
+        coupling=bsm.Coupling(cav=bsm.Cavity(bs0.root * root_rel), bs=bs),
         paraX=paraX,
         dipoleX=dipoleX,
     )
